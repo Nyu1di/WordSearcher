b6501822dc536e9d6ee9a05de3e996029d4880f0
17th Cat <juni7708@gmail.com>
2025-05-19 10:21:26 +0900

    Add files via upload

--- Main.py
+++ Main.py
@@ -19,15 +19,22 @@ driver = webdriver.Chrome(service=service, options=options) #백그라운드에 
 
 root = tk.Tk()
 root.title("성현성현 영사전")
-root.geometry("340x210")
+root.geometry("350x250")
 root.resizable(False, False)
 root.attributes('-topmost', True) #최우선 고정
 
 search = tk.Text(root, height=1, width=20)
 search.pack(pady=10)
 
-label = tk.Label(root, text="안녕 난 성현이라고 해.", wraplength=300) #wraplength는 텍스트 줄바꿈 제한. 
+label = tk.Label(root, text="안녕 난 성현이라고 해.", wraplength=300) #wraplength는 텍스트 줄바꿈 제한.
 label.pack()
+
+log = []
+def watch_log():
+    if not log:
+        return "검색 기록이 없습니다."
+    log_text = "\n".join(log)
+    return log_text
 
 def search_word(word):
     if not word:
@@ -42,6 +49,9 @@ def search_word(word):
     driver.get(f"https://dict.naver.com/search.dict?query={word}")
     meaning = driver.find_element(By.CLASS_NAME, 'addition_info').text
     driver.implicitly_wait(1)
+    if len(log) > 5:
+        log.pop(0)
+    log.append(word)
 
     return meaning
 
@@ -50,7 +60,10 @@ def preview(meaning):
     label.update()
     search.delete("1.0", tk.END)
 
-Button = tk.Button(root, text="Search", command=lambda: preview(search_word(search.get("1.0", tk.END).strip())))
-Button.pack(pady=10)
+SearchBtn = tk.Button(root, text="Search", command=lambda: preview(search_word(search.get("1.0", tk.END).strip())))
+SearchBtn.pack(pady=10)
+
+Searched_log = tk.Button(root, text="log", command=lambda: preview(watch_log()))
+Searched_log.place(x=320, y=210)
 
 root.mainloop()
